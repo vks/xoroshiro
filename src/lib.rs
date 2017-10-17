75c110ef7784a80bb148695e0ba780f5becc0edb
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2017-10-17 11:50:58 +0200

    Seeding from 64 bits using the splitmix64 RNG
    Also include various small improvements.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,75 @@ extern crate rand;
 
 use rand::{Rng, SeedableRng, Rand};
 
+/// A splitmix random number generator.
+///
+/// The splitmix algorithm is not suitable for cryptographic purposes, but is
+/// very fast and has a 64 bit state.  Usually `XoroShiroRng` should be prefered.
+/// If you do not know for sure that it fits your requirements, use a more
+/// secure one such as `IsaacRng` or `OsRng`.
+///
+/// The algorithm used here is translated from [the `splitmix64.c`
+/// reference source code](http://xorshift.di.unimi.it/splitmix64.c) by
+/// Sebastiano Vigna.
+#[allow(missing_copy_implementations)]
+#[derive(Debug, Clone)]
+pub struct SplitMixRng {
+    x: u64,
+}
+
+impl SplitMixRng {
+    /// Creates a new `SplitMixRng` instance which is not seeded.
+    ///
+    /// The initial values of this RNG are constants, so all generators created
+    /// by this function will yield the same stream of random numbers. It is
+    /// highly recommended that this is created through `SeedableRng` instead of
+    /// this function.
+    pub fn new_unseeded() -> SplitMixRng {
+        // The state can be seeded with any value.
+        SplitMixRng {
+            x: 0,
+        }
+    }
+}
+
+impl Rng for SplitMixRng {
+    #[inline]
+    fn next_u32(&mut self) -> u32 {
+        self.next_u64() as u32
+    }
+
+    #[inline]
+    fn next_u64(&mut self) -> u64 {
+        self.x += 0x9e3779b97f4a7c15;
+        let mut z = self.x;
+        z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
+        z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
+        return z ^ (z >> 31);
+    }
+}
+
+impl SeedableRng<u64> for SplitMixRng {
+    /// Reseed a `SplitMixRng`.
+    fn reseed(&mut self, seed: u64) {
+        self.x = seed;
+    }
+
+    /// Create a new `SplitMixRng`.
+    fn from_seed(seed: u64) -> SplitMixRng {
+        SplitMixRng {
+            x: seed,
+        }
+    }
+}
+
+impl Rand for SplitMixRng {
+    fn rand<R: Rng>(rng: &mut R) -> SplitMixRng {
+        SplitMixRng {
+            x: rng.gen(),
+        }
+    }
+}
+
 /// A xoroshiro+ random number generator.
 ///
 /// The xoroshiro+ algorithm is not suitable for cryptographic purposes, but
@@ -13,21 +82,21 @@ use rand::{Rng, SeedableRng, Rand};
 /// reference source code](http://xorshift.di.unimi.it/xoroshiro128plus.c) by
 /// David Blackman and Sebastiano Vigna.
 #[allow(missing_copy_implementations)]
-#[derive(Clone)]
+#[derive(Debug, Clone)]
 pub struct XoroShiroRng {
     s0: u64,
     s1: u64,
 }
 
 impl XoroShiroRng {
-    /// Creates a new XoroShiroRng instance which is not seeded.
+    /// Creates a new `XoroShiroRng` instance which is not seeded.
     ///
     /// The initial values of this RNG are constants, so all generators created
     /// by this function will yield the same stream of random numbers. It is
     /// highly recommended that this is created through `SeedableRng` instead of
-    /// this function
+    /// this function.
     pub fn new_unseeded() -> XoroShiroRng {
-        // These constants were taken from the XorShiftRng implementation above.
+        // These constants were taken from the `XorShiftRng` implementation.
         // The only requirement imposed by the algorithm is that these values
         // cannot be zero everywhere.
         XoroShiroRng {
@@ -54,18 +123,18 @@ impl Rng for XoroShiroRng {
 }
 
 impl SeedableRng<[u64; 2]> for XoroShiroRng {
-    /// Reseed an XoroShiroRng.  This will panic if `seed` is entirely 0.
+    /// Reseed an `XoroShiroRng`.  This will panic if `seed` is entirely 0.
     fn reseed(&mut self, seed: [u64; 2]) {
-        assert!(!seed.iter().all(|&x| x == 0),
-                "XoroShiroRng.reseed called with an all zero seed.");
+        assert!(seed != [0, 0],
+            "XoroShiroRng.reseed called with an all zero seed.");
 
         self.s0 = seed[0];
         self.s1 = seed[1];
     }
 
-    /// Create a new XoroShiroRng.  This will panic if `seed` is entirely 0.
+    /// Create a new `XoroShiroRng`.  This will panic if `seed` is entirely 0.
     fn from_seed(seed: [u64; 2]) -> XoroShiroRng {
-        assert!(!seed.iter().all(|&x| x == 0),
+        assert!(seed != [0, 0],
             "XoroShiroRng::from_seed called with an all zero seed.");
 
         XoroShiroRng {
@@ -75,15 +144,31 @@ impl SeedableRng<[u64; 2]> for XoroShiroRng {
     }
 }
 
+/// Use a RNG to generate a valid (non-zero) xoroshiro seed.
+fn generate_seed<R: Rng>(rng: &mut R) -> [u64; 2] {
+    let mut s: [u64; 2] = rng.gen();
+    while s == [0, 0] {
+        s = rng.gen();
+    }
+    s
+}
+
+impl SeedableRng<u64> for XoroShiroRng {
+    /// Reseed an `XoroShiroRng`.  This will use `SplitMixRng` to fill the seed.
+    fn reseed(&mut self, seed: u64) {
+        let mut rng = SplitMixRng::from_seed(seed);
+        self.reseed(generate_seed(&mut rng));
+    }
+
+    /// Create a new `XoroShiroRng`.  This will use `SplitMixRng` to fill the seed.
+    fn from_seed(seed: u64) -> XoroShiroRng {
+        let mut rng = SplitMixRng::from_seed(seed);
+        XoroShiroRng::from_seed(generate_seed(&mut rng))
+    }
+}
+
 impl Rand for XoroShiroRng {
     fn rand<R: Rng>(rng: &mut R) -> XoroShiroRng {
-        let mut seed: [u64; 2] = rng.gen();
-        while seed == [0, 0] {
-            seed = rng.gen();
-        }
-        XoroShiroRng {
-            s0: seed[0],
-            s1: seed[1],
-        }
+        XoroShiroRng::from_seed(generate_seed(rng))
     }
 }
